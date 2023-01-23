5ba8844015f8e30dc45f2fad7a88e6c0a0e3b678
Isaac <isaac.lin.pnc@gmail.com>
2023-01-23 11:26:05 -0500

    finished lab.

--- src/index.js
+++ src/index.js
@@ -13,3 +13,75 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+document.querySelector(".add-toy-form").addEventListener("submit", handleSubmit)
+
+function handleSubmit(e) {
+  e.preventDefault()
+  let toyObj = {
+    name: e.target.name.value,
+    image: e.target.image.value,
+    likes: 0
+  }
+  renderOneToy(toyObj)
+  postToy(toyObj)
+}
+
+function renderOneToy(toy) {
+  //Build toy
+  let card = document.createElement("div")
+  card.className = "card"
+  card.innerHTML = `
+    <h2>${toy.name}</h2>
+    <img src= "${toy.image}" class = "toy-avatar"/>
+    <p>${toy.likes}</p>
+    <button class="like-btn" id=${toy.id}>Like ❤️</button>
+    `
+  card.querySelector(".like-btn").addEventListener("click", () => {
+    toy.likes++
+    card.querySelector("p").textContent = toy.likes
+    updateLikes(toy)
+  })
+
+    //Add toy card to DOM
+  const toyCollection = document.querySelector("#toy-collection")
+  toyCollection.appendChild(card)
+}
+
+
+
+function getAllToys(){
+fetch ("http://localhost:3000/toys")
+.then(response => response.json())
+.then(toyData => {
+  toyData.forEach(toy => renderOneToy(toy))})
+}
+
+getAllToys()
+
+
+function postToy(toyObj) {
+  fetch("http://localhost:3000/toys", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json"
+    },
+    body: JSON.stringify(toyObj)
+  })
+  .then(response => response.json())
+  .then(toy => console.log(toy))
+}
+
+function updateLikes(toy) {
+  fetch(`http://localhost:3000/toys/${toy.id}`, {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json"
+    },
+    body: JSON.stringify(toy)
+  })
+  .then(response => response.json())
+  .then(toy => console.log(toy))
+}
